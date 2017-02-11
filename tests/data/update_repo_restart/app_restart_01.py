c7f8d6680d95ec13141cf8604d45bbb2caa09ed5
Mengyang Li <mayli.he@gmail.com>
2017-02-10 18:37:22 -0800

    fixup unittest for using custom dir as update_folder

--- tests/data/update_repo_restart/app_restart_01.py
+++ tests/data/update_repo_restart/app_restart_01.py
@@ -25,8 +25,12 @@ def cb(status):
 
 def main():
     print(VERSION)
-    client = Client(client_config.ClientConfig(),
-                    refresh=True, progress_hooks=[cb])
+    data_dir= None
+    config = client_config.ClientConfig()
+    if getattr(config, 'USE_CUSTOM_DIR', False):
+        data_dir = os.path.join(os.path.dirname(sys.executable), '.update')
+    client = Client(config,
+                    refresh=True, progress_hooks=[cb], data_dir=data_dir)
     update = client.update_check(APPNAME, VERSION)
     if update is not None:
         success = update.download()
