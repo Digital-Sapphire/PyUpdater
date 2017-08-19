563d3a566ba60e5659e98760318a3f44f8ae844b
James Hilliard <james.hilliard1@gmail.com>
2017-08-19 19:19:45 +0300

    Add attribute checks for some pyinstaller options that aren't always present and fix command detection in dispatch_command

--- pyupdater/cli/__init__.py
+++ pyupdater/cli/__init__.py
@@ -90,13 +90,13 @@ def _real_main(args, namespace_test_helper=None):  # pragma: no cover
 def dispatch_command(args, pyi_args=None, test=False):
     # Turns collect-debug-info into collect_debug_info
     cmd_str = "_cmd_" + args.command.replace('-', '_')
-    try:
+    if hasattr(commands, cmd_str):
         cmd = getattr(commands, cmd_str)
         # We are just making sure we can load the function
         if test:
             return True
         cmd(args, pyi_args)
-    except AttributeError:
+    else:
         # This should only get hit by misconfigured tests.
         # "Should" being the key word here :)
         log.error('Unknown Command: %s', cmd_str)
